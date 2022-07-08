118f29947d699dbfb94a54248b53a7c9491b545b
Malcolm Smith <malcolms65@gmail.com>
2022-07-08 17:50:54 +1000

    Initial Commit

---
+++ js/counting.js
@@ -0,0 +1,55 @@
+function displayWelcome() {
+  const name = getCookie("kidsname");
+  const tmp = document.getElementById("kidsname");
+  tmp.textContent = "Hello " + name + " lets start counting!!";
+}
+
+let i = 1;
+numWords = [
+  "ONE",
+  "TWO",
+  "THREE",
+  "FOUR",
+  "FIVE",
+  "SIX",
+  "SEVEN",
+  "EIGHT",
+  "NINE",
+  "TEN",
+];
+
+function startCounting() {
+  console.info("Start counting...");
+  const congrats = document.querySelector("#congrats");
+  congrats.innerText = "";
+  const imgDiv = document.querySelector("#imgDiv");
+  clearElement(imgDiv);
+  run(imgDiv);
+}
+
+function run(imgDiv) {
+  const numDiv = document.querySelector("#numDiv");
+  const img = document.createElement("img");
+  img.src = "/images/dog.png";
+  imgDiv.appendChild(img);
+  numDiv.innerHTML =
+    "<div id='num'>" +
+    i +
+    "</div><div id='numWord'>" +
+    numWords[i - 1] +
+    "</div>";
+
+  setTimeout(() => {
+    i++;
+    if (i <= 10) run(imgDiv);
+    else {
+      i = 1;
+      congrats();
+    }
+  }, 1000);
+}
+
+function congrats() {
+  const congrats = document.querySelector("#congrats");
+  congrats.innerText = "Well Done " + getCookie("kidsname") + "!!!";
+}
---
+++ js/javascript.js
@@ -0,0 +1,45 @@
+function setCookie(name, value) {
+  document.cookie = name + "=" + escape(value); // + "; path=/; expires=" + expiry.toGMTString();
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const name = parts.pop().split(";").shift();
+    return name;
+  }
+}
+
+window.addEventListener("load", function () {
+  //alert("Hi");
+  function processForm() {
+    const kidsname = document.getElementById("kidsname").value;
+    setCookie("kidsname", kidsname);
+  }
+
+  const name = getCookie("kidsname");
+  if (name) {
+    const kidsname = document.getElementById("kidsname");
+    kidsname.value = name;
+  }
+
+  // Access the form element...
+  const form = document.getElementById("userNameForm");
+
+  if (!form) return;
+
+  // ...and take over its submit event.
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    processForm();
+    window.location = "/counting.html";
+  });
+});
+
+function clearElement(imgDiv) {
+  while (imgDiv.firstChild) {
+    imgDiv.removeChild(imgDiv.firstChild);
+  }
+}
